d6cb5ac5424a3f7cfc3a2f444a3afde320f0e45b
giovannicampa <giocampa93@gmail.com>
2021-01-19 20:48:38 +0100

    Added hyperparameter search

--- Prediction_model_TF.py
+++ Prediction_model_TF.py
@@ -3,10 +3,13 @@
 import os
 import sys
 
+from itertools import product
+import datetime
+
 import numpy as np
 import pandas as pd
 import math
-import datetime
+
 import matplotlib.pyplot as plt
 
 import tensorflow as tf
@@ -17,12 +20,14 @@ from tensorflow.keras.callbacks import LearningRateScheduler
 from tensorflow.keras.models import Model
 
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from sklearn.model_selection import GridSearchCV, train_test_split
+from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
 
 from data_preparation import prepare_data
 
 
-tf.config.experimental.set_visible_devices([], 'GPU')
+tf.config.experimental.set_visible_devices([], 'GPU') # Enforcing the usage of the CPU
+
 
 # ---------------------------------------------------------------------------------------------------------------
 # - Loading the data
@@ -55,95 +60,124 @@ X_train, X_test, y_train, y_test = train_test_split(X,y)
 # ---------------------------------------------------------------------------------------------------------------
 # - Callbacks
 
-
 # Tensorboard callback
 
 current_path = os.path.dirname(os.path.realpath(__file__))
 now = datetime.datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S").strip(" ").replace("/", "_").replace(":", "_").replace(" ", "_")
 
-log_directory = current_path + "/tensorboard/"+ dt_string
+# log_directory = current_path + "/tensorboard/"+ dt_string
 
-
-tb_callback = TensorBoard(log_dir = log_directory)
-
+# tb_callback = TensorBoard(log_dir = log_directory)
 
 
 # Learning rate scheduler
 
-initial_learning_rate = 0.01
-def lr_step_decay(epoch, lr):
-    drop_rate = 0.5
-    epochs_drop = 10.0
-    return initial_learning_rate * math.pow(drop_rate, math.floor(epoch/epochs_drop))
+def generate_lr_scheduler(initial_learning_rate = 0.01):
 
-lr_callback = LearningRateScheduler(lr_step_decay)
+    def lr_step_decay(epoch, lr):
+        drop_rate = 0.5
+        epochs_drop = 10.0
+        return initial_learning_rate * math.pow(drop_rate, math.floor(epoch/epochs_drop))
+
+    return LearningRateScheduler(lr_step_decay)
 
 
 # ---------------------------------------------------------------------------------------------------------------
 # - Building the TF Model
 
-# model = tf.keras.layer.Sequential([
-#     tf.keras.layers.Dense(8, activation="relu", input_shape=[X.shape[1]]),
-#     tf.keras.layers.Dense(8, activation="relu"),
-#     tf.keras.layers.Dense(8, activation="relu"),
-#     tf.keras.layers.Dense(8, activation="relu"),
-#     tf.keras.layers.Dense(2)
-# ])
+def build_model(neurons = 8, depth = 3, activation = "relu", optimizer = "RMSprop", learning_rate = 0.01):
+    """ GridSearch compatible model definition
+    """
+    
+    # ---------------------------------------------------------------------------------------------------------------
+    # - Callbacks
+    
+    # Tensorboard
+    log_directory = current_path + "/tensorboard/"+ \
+                    "_neu_" + str(neurons) + \
+                    "_dep_" + str(depth) + \
+                    "_act_" + str(activation) + \
+                    "_opt_" + str(optimizer)
 
-input_layer = Input(shape=[X.shape[1]])
-x = Dense(units = 8)(input_layer)
-x = Dense(units = 8)(x)
-x = Dense(units = 8)(x)
-
-x_1 = Dense(units = 4)(x)
-out_1 = Dense(units = 1)(x_1)
-
-x_2 = Dense(units = 4)(x)
-out_2 = Dense(units = 1)(x_2)
-
-model = Model(inputs = input_layer, outputs = [out_1, out_2])
+    tb_callback = TensorBoard(log_dir = log_directory)
 
 
-
-optimizer = tf.keras.optimizers.RMSprop(0.001)
-
-model.compile(
-        loss='mean_squared_error',
-        optimizer=optimizer,
-        metrics=['mean_squared_error'])
-
-history = model.fit(
-        X_train,
-        y_train.loc[:,['score_home', 'score_away']],
-        epochs=40,
-        batch_size=8,
-        validation_split = 0.3,
-        verbose=1,
-        callbacks=[tb_callback, lr_callback])
+    # Learning rate scheduler
+    lr_callback = generate_lr_scheduler(initial_learning_rate= learning_rate)
 
 
-# ---------------------------------------------------------------------------------------------------------------
-# - Evaluation
+    # ---------------------------------------------------------------------------------------------------------------
+    # - Model definition
+    model = tf.keras.models.Sequential()
 
-# Evaluating the regression model (predicts nr of goals for each team) with regard to the winning team
-# To do this, from the predicted scores the winning team must be calculated. This is then compared with
-# the actual result of the match
+    # Input layer
+    model.add(tf.keras.layers.Dense(neurons, activation=activation, input_shape=[X.shape[1]]))
 
-y_predict = model.predict(X_test)
+    # Hidden layers
+    for _ in range(depth):
+        model.add(tf.keras.layers.Dense(neurons, activation=activation))
 
-y_predict_home = y_predict[0]
-y_predict_away = y_predict[1]
-y_predict.columns = ['score_home', 'score_away']
-predicted_winners = np.empty([y_predict.shape[0],1], dtype = 'str')
-predicted_winners[y_predict.score_home > y_predict.score_away] = 'H'
-predicted_winners[y_predict.score_home < y_predict.score_away] = 'A'
-predicted_winners[np.round(y_predict.score_home) == np.round(y_predict.score_away)] = 'D'
+    # Output layer
+    model.add(tf.keras.layers.Dense(2))
 
-real_winners = y_test.loc[:, 'winners'].values.tolist()
+    model.compile(
+            loss = "mse",
+            metrics = "mean_squared_error",
+            optimizer=optimizer)
+
+    model.fit(
+            X_train,
+            y_train.loc[:,['score_home', 'score_away']],
+            epochs=50,
+            batch_size=8,
+            validation_split = 0.3,
+            verbose=0,
+            callbacks=[tb_callback, lr_callback])
 
 
-# - Evaluating the regressor by comparing the predicted and the real winner
-nr_games = predicted_winners.shape[0]
+    # ---------------------------------------------------------------------------------------------------------------
+    # - Evaluation
 
-print('Prediction accuracy: ',sum(predicted_winners.reshape(1,-1)[0] == real_winners)/nr_games*100)
+    # Evaluating the regression model (predicts nr of goals for each team) with regard to the winning team
+    # To do this, from the predicted scores the winning team must be calculated. This is then compared with
+    # the actual result of the match
+
+    y_predict = np.array(model.predict(X_test))
+
+    y_predict_home = y_predict[:,0] # Scores of the home team
+    y_predict_away = y_predict[:,1] # Scores of the away team
+
+    predicted_result = np.empty([len(y_predict_home),1], dtype = 'str')
+    predicted_result[np.greater(y_predict_home,y_predict_away)] = 'H'              # Home team wins
+    predicted_result[y_predict_home < y_predict_away] = 'A'                        # Away team wins
+    predicted_result[np.round(y_predict_home) == np.round(y_predict_away)] = 'D'   # Draw
+
+    real_result = y_test.loc[:, 'winners'].values.tolist()                          # Which team (H,A,D) has really won
+
+    # - Evaluating the regressor by comparing the predicted and the real winner
+    nr_games = predicted_result.shape[0]
+
+    print('Prediction accuracy: ',sum(predicted_result.reshape(1,-1)[0] == real_result)/nr_games*100)
+
+
+
+param_grid = {"depth":[2,4,8],
+                "activation": ["relu", "tanh"],
+                "neurons":[16,64,128],
+                "learning_rate": [0.1, 0.01, 0.01]}
+
+# List of 
+combinations_of_params = [dict(zip(param_grid, v)) for v in product(*param_grid.values())]
+
+for parameter in combinations_of_params:
+
+    build_model(optimizer = parameter["optimizer"],
+                depth = parameter["depth"],
+                activation = parameter["activation"],
+                neurons = parameter["neurons"],
+                learning_rate=parameter["learning_rate"])
+
+
+
+
