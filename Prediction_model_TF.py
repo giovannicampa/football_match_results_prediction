20838783e674ec90e0db5370ccf3686cf553810a
giovannicampa <giocampa93@gmail.com>
2021-01-20 11:16:40 +0100

    Removed tb logs, keeping track of best losses

--- Prediction_model_TF.py
+++ Prediction_model_TF.py
@@ -18,6 +18,7 @@ from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.python.eager import context
 from tensorflow.keras.callbacks import LearningRateScheduler
 from tensorflow.keras.models import Model
+from tensorflow.keras import backend as K
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import GridSearchCV, train_test_split
@@ -27,6 +28,7 @@ from data_preparation import prepare_data
 
 
 tf.config.experimental.set_visible_devices([], 'GPU') # Enforcing the usage of the CPU
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 
 # ---------------------------------------------------------------------------------------------------------------
@@ -37,7 +39,7 @@ if os.path.exists("data.csv"):
 else:
     data = prepare_data()
 
-X  = data.drop(['score_home','score_away', 'winners'], axis = 1, inplace = False)
+X = data.drop(['score_home','score_away', 'winners'], axis = 1, inplace = False)
 y = data.loc[:, ['score_home','score_away', 'winners']] 
 
 
@@ -60,15 +62,11 @@ X_train, X_test, y_train, y_test = train_test_split(X,y)
 # ---------------------------------------------------------------------------------------------------------------
 # - Callbacks
 
-# Tensorboard callback
+# Tensorboard callback (string name only)
 
 current_path = os.path.dirname(os.path.realpath(__file__))
 now = datetime.datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S").strip(" ").replace("/", "_").replace(":", "_").replace(" ", "_")
-
-# log_directory = current_path + "/tensorboard/"+ dt_string
-
-# tb_callback = TensorBoard(log_dir = log_directory)
 
 
 # Learning rate scheduler
@@ -85,99 +83,116 @@ def generate_lr_scheduler(initial_learning_rate = 0.01):
 
 # ---------------------------------------------------------------------------------------------------------------
 # - Building the TF Model
+class ModelBuilder():
 
-def build_model(neurons = 8, depth = 3, activation = "relu", optimizer = "RMSprop", learning_rate = 0.01):
-    """ GridSearch compatible model definition
-    """
-    
-    # ---------------------------------------------------------------------------------------------------------------
-    # - Callbacks
-    
-    # Tensorboard
-    log_directory = current_path + "/tensorboard/"+ \
-                    "_neu_" + str(neurons) + \
-                    "_dep_" + str(depth) + \
-                    "_act_" + str(activation) + \
-                    "_opt_" + str(optimizer)
+    def __init__(self):
+        self.best_loss = 100
 
-    tb_callback = TensorBoard(log_dir = log_directory)
+    def build(self, neurons = 8, depth = 3, activation = "relu", optimizer = "RMSprop", learning_rate = 0.01, batch_size = 8):
+        """ GridSearch compatible model definition
+        """
+        
+        # ---------------------------------------------------------------------------------------------------------------
+        # - Callbacks
+        
+        # Tensorboard
+        self.hp_string = "_nr_" + str(neurons) + \
+                         "_depth_" + str(depth) + \
+                         "_act_" + str(activation) + \
+                         "_opt_" + str(optimizer) + \
+                         "_batch_"+str(batch_size)
 
-
-    # Learning rate scheduler
-    lr_callback = generate_lr_scheduler(initial_learning_rate= learning_rate)
-
-
-    # ---------------------------------------------------------------------------------------------------------------
-    # - Model definition
-    model = tf.keras.models.Sequential()
-
-    # Input layer
-    model.add(tf.keras.layers.Dense(neurons, activation=activation, input_shape=[X.shape[1]]))
-
-    # Hidden layers
-    for _ in range(depth):
-        model.add(tf.keras.layers.Dense(neurons, activation=activation))
-
-    # Output layer
-    model.add(tf.keras.layers.Dense(2))
-
-    model.compile(
-            loss = "mse",
-            metrics = "mean_squared_error",
-            optimizer=optimizer)
-
-    model.fit(
-            X_train,
-            y_train.loc[:,['score_home', 'score_away']],
-            epochs=50,
-            batch_size=8,
-            validation_split = 0.3,
-            verbose=0,
-            callbacks=[tb_callback, lr_callback])
+        log_directory = current_path + "/tensorboard/"+ self.hp_string
 
 
-    # ---------------------------------------------------------------------------------------------------------------
-    # - Evaluation
+        tb_callback = TensorBoard(log_dir = log_directory, profile_batch=0)
 
-    # Evaluating the regression model (predicts nr of goals for each team) with regard to the winning team
-    # To do this, from the predicted scores the winning team must be calculated. This is then compared with
-    # the actual result of the match
 
-    y_predict = np.array(model.predict(X_test))
+        # Learning rate scheduler
+        lr_callback = generate_lr_scheduler(initial_learning_rate= learning_rate)
 
-    y_predict_home = y_predict[:,0] # Scores of the home team
-    y_predict_away = y_predict[:,1] # Scores of the away team
 
-    predicted_result = np.empty([len(y_predict_home),1], dtype = 'str')
-    predicted_result[np.greater(y_predict_home,y_predict_away)] = 'H'              # Home team wins
-    predicted_result[y_predict_home < y_predict_away] = 'A'                        # Away team wins
-    predicted_result[np.round(y_predict_home) == np.round(y_predict_away)] = 'D'   # Draw
+        # ---------------------------------------------------------------------------------------------------------------
+        # - Model definition
+        self.model = tf.keras.models.Sequential()
 
-    real_result = y_test.loc[:, 'winners'].values.tolist()                          # Which team (H,A,D) has really won
+        # Input layer
+        self.model.add(tf.keras.layers.Dense(neurons, activation=activation, input_shape=[X.shape[1]]))
 
-    # - Evaluating the regressor by comparing the predicted and the real winner
-    nr_games = predicted_result.shape[0]
+        # Hidden layers
+        for _ in range(depth):
+            self.model.add(tf.keras.layers.Dense(neurons, activation=activation))
 
-    print('Prediction accuracy: ',sum(predicted_result.reshape(1,-1)[0] == real_result)/nr_games*100)
+        # Output layer
+        self.model.add(tf.keras.layers.Dense(2))
+
+        self.model.compile(
+                loss = "mse",
+                metrics = "mean_squared_error",
+                optimizer=optimizer)
+
+        history = self.model.fit(
+                X_train,
+                y_train.loc[:,['score_home', 'score_away']],
+                epochs=50,
+                batch_size=batch_size,
+                validation_split = 0.3,
+                verbose=0,
+                callbacks=[tb_callback, lr_callback])
+
+        self.current_loss = np.min(history.history["loss"])
+
+
+    def evaluate_model(self):
+        """Evaluating the regression model
+
+        predicts nr of goals for each team with regard to the winning team
+        To do this, from the predicted scores the winning team must be calculated.
+        This is then compared with the actual result of the match
+        """
+
+        y_predict = np.array(self.model.predict(X_test))
+
+        y_predict_home = y_predict[:,0] # Scores of the home team
+        y_predict_away = y_predict[:,1] # Scores of the away team
+
+        predicted_result = np.empty([len(y_predict_home),1], dtype = 'str')
+        predicted_result[np.greater(y_predict_home,y_predict_away)] = 'H'              # Home team wins
+        predicted_result[y_predict_home < y_predict_away] = 'A'                        # Away team wins
+        predicted_result[np.round(y_predict_home) == np.round(y_predict_away)] = 'D'   # Draw
+
+        real_result = y_test.loc[:, 'winners'].values.tolist()                          # Which team (H,A,D) has really won
+
+        # - Evaluating the regressor by comparing the predicted and the real winner
+        nr_games = predicted_result.shape[0]
+
+        if self.current_loss < self.best_loss:
+            self.best_loss = self.current_loss
+            print(f'Best loss ({self.best_loss}) is for {self.hp_string}. prediction accuracy: {sum(predicted_result.reshape(1,-1)[0] == real_result)/nr_games*100}')
 
 
 
 param_grid = {"depth":[2,4,8],
                 "activation": ["relu", "tanh"],
                 "neurons":[16,64,128],
-                "learning_rate": [0.1, 0.01, 0.01]}
+                "learning_rate": [0.1, 0.01, 0.01],
+                "batch_size":[32,64]}
 
-# List of 
+# List of parameter combinations
 combinations_of_params = [dict(zip(param_grid, v)) for v in product(*param_grid.values())]
+
+
+model_builder = ModelBuilder()
 
 for parameter in combinations_of_params:
 
-    build_model(optimizer = parameter["optimizer"],
-                depth = parameter["depth"],
-                activation = parameter["activation"],
-                neurons = parameter["neurons"],
-                learning_rate=parameter["learning_rate"])
-
+    model_builder.build(depth = parameter["depth"],
+                        activation = parameter["activation"],
+                        neurons = parameter["neurons"],
+                        learning_rate=parameter["learning_rate"],
+                        batch_size=parameter["batch_size"])
+    
+    model_builder.evaluate_model()
 
 
 
