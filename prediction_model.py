7022bed6cd16f11c6b1f501b3efcd388efaa0cf9
giovannicampa <giocampa93@gmail.com>
2021-01-24 23:51:59 +0100

    Testing multiple values for feature importance threshold

--- prediction_model.py
+++ prediction_model.py
@@ -6,6 +6,8 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import mean_squared_error, make_scorer
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from sklearn.metrics import accuracy_score
+
 import numpy as np
 import pandas as pd
 
@@ -27,10 +29,10 @@ X  = data.drop(['score_home','score_away', 'winners'], axis = 1, inplace = False
 y = data.loc[:, ['score_home','score_away', 'winners']] 
 
 random_forest = RandomForestRegressor(n_jobs = -1)
-neural_network = MLPRegressor(activation = 'logistic')
+neural_network = MLPRegressor(activation = 'relu', solver = "adam", early_stopping= True)
 
-param_grid_rf = {'max_depth':list(range(2,10)), 'max_features': list(range(5,X.shape[1])), 'n_estimators': list(range(70,200,10))}
-param_grid_mlp = {'hidden_layer_sizes': list(range(20,100,7))}
+param_grid_rf = {'max_depth':list(range(3,10,2)), 'max_features': list(range(5,X.shape[1])), 'n_estimators': list(range(70,200,10))}
+param_grid_mlp = {'hidden_layer_sizes': list(range(8,128,32))}
 
 
 def find_best_params(regressor, param_grid, X, y):
@@ -96,36 +98,41 @@ print('Prediction accuracy with all features: ',sum(predicted_winners.reshape(1,
 # - 2.4: Evaluate the regressor with the reduced features
 
 
-# - 2.1: Finding the most important features and retraining the model on them only
-feature_relevance = (regressor_rf.feature_importances_>0.01)
-feature_names = X.columns[feature_relevance]
+range_feature_importances = np.linspace(0.01, 0.2, 5)
+
+for feat_importance in range_feature_importances:
+    # - 2.1: Finding the most important features and retraining the model on them only
+    feature_relevance = (regressor_rf.feature_importances_>feat_importance)
+    feature_names = X.columns[feature_relevance]
 
 
-# - 2.2: Remove the not important features from the feature set
-X_reduced = X.loc[:, feature_names]
+    # - 2.2: Remove the not important features from the feature set
+    X_reduced = X.loc[:, feature_names]
 
-# Splitting in training and test sets
-Xred_train, Xred_test, y_train, y_test = train_test_split(X_reduced, y)
-
-
-# - 2.3: Find the best parameters for the regressors
-regressor_mlp = find_best_params(neural_network, param_grid_mlp, X_reduced, y)
-regressor_mlp.fit(Xred_train, y_train.drop('winners', axis = 1))
+    # Splitting in training and test sets
+    Xred_train, Xred_test, y_train, y_test = train_test_split(X_reduced, y)
 
 
-# - 2.4: Instead of prediction the outcome as Home wins, Away wins.., we predict the scores and then we calculate the winner
-
-y_predict = pd.DataFrame(regressor_mlp.predict(Xred_test))
-y_predict.columns = ['score_home', 'score_away']
-
-predicted_winners = np.empty([y_predict.shape[0],1], dtype = 'str')
-predicted_winners[np.round(y_predict.score_home) > np.round(y_predict.score_away)] = 'H'
-predicted_winners[np.round(y_predict.score_home) < np.round(y_predict.score_away)] = 'A'
-predicted_winners[np.round(y_predict.score_home) == np.round(y_predict.score_away)] = 'D'
-
-real_winners = y_test.loc[:, 'winners'].values.tolist()
+    # - 2.3: Find the best parameters for the regressors
+    regressor_mlp = find_best_params(neural_network, param_grid_mlp, X_reduced, y)
+    regressor_mlp.fit(Xred_train, y_train.drop('winners', axis = 1))
 
 
-nr_games = predicted_winners.shape[0]
+    # - 2.4: Instead of prediction the outcome as Home wins, Away wins.., we predict the scores and then we calculate the winner
 
-print('Prediction accuracy with most relevant features: ',sum(predicted_winners.reshape(1,-1)[0] == real_winners)/nr_games*100)
+    y_predict = pd.DataFrame(regressor_mlp.predict(Xred_test))
+    y_predict.columns = ['score_home', 'score_away']
+
+    predicted_winners = np.empty([y_predict.shape[0],1], dtype = 'str')
+    predicted_winners[np.round(y_predict.score_home) > np.round(y_predict.score_away)] = 'H'
+    predicted_winners[np.round(y_predict.score_home) < np.round(y_predict.score_away)] = 'A'
+    predicted_winners[np.round(y_predict.score_home) == np.round(y_predict.score_away)] = 'D'
+
+    real_winners = y_test.loc[:, 'winners'].values.tolist()
+
+
+    nr_games = predicted_winners.shape[0]
+
+    prediction_accuracy = accuracy_score(predicted_winners.reshape(1,-1)[0], real_winners)
+
+    print(f'Prediction accuracy with most relevant features: {prediction_accuracy:.2f} with features: {feature_names}')
